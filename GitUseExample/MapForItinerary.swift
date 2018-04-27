26bf2fcf0d3a513b066a2a6b6c4a479331c3104b
appCradle <mike@appcradle.eu>
2018-04-27 10:30:30 +0200

    Add new map-for-itineray function

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Mike Neirinck on 27/04/18.
+//  Copyright © 2018 appCradle. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
